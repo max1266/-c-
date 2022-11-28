cbf7763976f3946697276423bd66546270613fef
max1266 <118952053+max1266@users.noreply.github.com>
2022-11-29 01:55:48 +0700

    программа на определение честности числа

--- Program.cs
+++ Program.cs
@@ -1,17 +1,10 @@
-﻿Console.Clear();
-Console.WriteLine("Введите первое число");
+﻿//программа на определение четности числа
+Console.Clear();
+Console.WriteLine("Введите число");
 double a = Convert.ToDouble(Console.ReadLine());
-Console.WriteLine("Введите второе число");
-double b = Convert.ToDouble(Console.ReadLine());
-Console.WriteLine("Введите третье число");
-double c = Convert.ToDouble(Console.ReadLine());
-double max = a ;
-if (a > max)
-    max = a;
-if (b > max)
-    max = b;
-    else
-        max = a;
-if (c > max)
-    max = c;
-Console.WriteLine(max);
+if (a%2 ==0){
+    Console.WriteLine("да");
+}
+else {
+    Console.WriteLine("Нет");
+}
