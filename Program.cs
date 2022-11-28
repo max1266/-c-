3075759420ab7c8d0ce30f82272baf7c076f52f9
Maxim Shmidt <history1266@mail.ru>
2022-11-29 00:30:25 +0700

    Программа для нахождения макс и мин числа

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.WriteLine("Введите первое число");
+double numberA = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+double numberB = Convert.ToDouble(Console.ReadLine());
+if (numberA > numberB){
+    Console.Write("Максимальное число = ");
+    Console.Write(numberA);
+    Console.Write("Минимальное число = ");
+    Console.Write(numberB);
+}
+else {
+Console.Write("Максимальное число = ");
+Console.Write(numberB);
+Console.Write("Минимальное число = ");
+Console.Write(numberA);
+}
